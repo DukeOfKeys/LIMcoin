fbf8b05e345fd4822d3c0879368810c7c5e62401
Kiryl Yasinsky <kirillinski@icloud.com>
2023-10-22 15:06:55 +0300

    Initial Commit

---
+++ LIMcoin/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LIMcoin
+//
+//  Created by Kiryl Yasinskiy on 10/22/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
